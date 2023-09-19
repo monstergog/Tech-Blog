c951de557d98c5fee9fd01ff43a89067e0522110
monstergog <aminhn95@gmail.com>
2023-09-18 21:26:04 -0700

    fixing uppercased file issue

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/');
 const homeRoutes = require('./homeRoutes.js');
-const dashboardRoutes = require('./dashboardRoutes.js');
+const dashboardroutes = require('./dashboardroutes.js');
 
 router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
+router.use('/dashboard', dashboardroutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
